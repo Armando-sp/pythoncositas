654b725276d9901f18d0e0dc34d2ec88f98e5af8
Armando Svivira <armandosivira10@gmail.com>
2022-03-18 15:51:14 -0400

    arreglo de error

--- conversor.py
+++ conversor.py
@@ -1,7 +1,7 @@
 def run():
     bolivares= float(input('Cuantos bolivares tienes?: '))
     precio_dolar= float(4.44)
-    dolares= round(bolivares/ precio_dolar, 2)
+    dolares= str(round(bolivares/ precio_dolar, 2))
     print('tienes '+ dolares + ' $')
 
 
