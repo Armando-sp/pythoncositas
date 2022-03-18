4f513ad0697a1e1f0b308c025c375b3a233ae381
Armando Svivira <armandosivira10@gmail.com>
2022-03-18 15:59:26 -0400

    cambio en el print

--- conversor.py
+++ conversor.py
@@ -2,7 +2,7 @@ def run():
     bolivares= float(input('Cuantos bolivares tienes?: '))
     precio_dolar= float(4.44)
     dolares= str(round(bolivares/ precio_dolar, 2))
-    print('tienes '+ dolares + ' $')
+    print('posees '+ dolares + ' $')
 
 
 if __name__ == '__main__':
