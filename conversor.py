a8bec5b18fbb346f4fcdb56af7cb1c1b27f1582b
Armando Svivira <armandosivira10@gmail.com>
2022-03-20 14:22:23 -0400

    cambio en el tipo de moneda

--- conversor.py
+++ conversor.py
@@ -1,5 +1,5 @@
 def run():
-    bolivares= float(input('Cuantos bolivares tienes?: '))
+    bolivares= float(input('Cuantos pesos tienes?: '))
     precio_dolar= float(4.5)
     dolares= str(round(bolivares/ precio_dolar, 2))
     print('tienes '+ dolares + ' $')
